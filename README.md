4b1e8be5948653e84129cb27719fb612bc59bdd9
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2023-07-07 17:56:29 -0300

    .

--- README.md
+++
@@ -1,3 +0,0 @@
-# test
-test
-https://leonardobdev.github.io/test
--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-const registerServiceWorker = async () => {
-	if ("serviceWorker" in navigator) {
-	  try {
-		const registration = await navigator.serviceWorker.register("sw.js");
-		if (registration.installing) {
-		  console.log("Service worker installing");
-		} else if (registration.waiting) {
-		  console.log("Service worker installed");
-		} else if (registration.active) {
-		  console.log("Service worker active");
-		}
-	  } catch (error) {
-		console.error(`Registration failed with ${error}`);
-	  }
-	}
-  };
-  registerServiceWorker();
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/sw.js')
+	.then(function () {
+		console.log('service worker registered');
+	})
+	.catch(function () {
+		console.warn('service worker failed');
+	});
+}
