a1cc9b5485cb02fd4236b0663fa7cbcf2a21018d
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2023-07-07 17:24:00 -0300

    .

--- sw.js
+++ sw.js
@@ -1,52 +1,39 @@
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open("v1");
-  await cache.addAll(resources);
-};
+var CACHE_NAME = 'static-v1';
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    addResourcesToCache([
-      "/",
-      "/index.html",
-      "/style.css",
-      "/app.js",
-      "/image-list.js",
-      "/star-wars-logo.jpg",
-      "/gallery/bountyHunters.jpg",
-      "/gallery/myLittleVader.jpg",
-      "/gallery/snowTroopers.jpg",
-    ])
-  );
+self.addEventListener('install', function (event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(function (cache) {
+			return cache.addAll([
+				'/',
+				'/index.html',
+				'/src/css/style.css',
+				'/src/img/logo.svg',
+				'/app.js',
+				'/manifest.js',
+			]);
+		})
+	)
 });
-const putInCache = async (request, response) => {
-  const cache = await caches.open("v1");
-  await cache.put(request, response);
-};
 
-const cacheFirst = async (request) => {
-  const responseFromCache = await caches.match(request);
-  if (responseFromCache) {
-    return responseFromCache;
-  }
-  const responseFromNetwork = await fetch(request);
-  putInCache(request, responseFromNetwork.clone());
-  return responseFromNetwork;
-};
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(cacheFirst(event.request));
+self.addEventListener('activate', function activator(event) {
+	event.waitUntil(
+		caches.keys().then(function (keys) {
+			return Promise.all(keys
+				.filter(function (key) {
+					return key.indexOf(CACHE_NAME) !== 0;
+				})
+				.map(function (key) {
+					return caches.delete(key);
+				})
+			);
+		})
+	);
 });
-const deleteCache = async (key) => {
-  await caches.delete(key);
-};
 
-const deleteOldCaches = async () => {
-  const cacheKeepList = ["v2"];
-  const keyList = await caches.keys();
-  const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
-  await Promise.all(cachesToDelete.map(deleteCache));
-};
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(deleteOldCaches());
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request).then(function (cachedResponse) {
+			return cachedResponse || fetch(event.request);
+		})
+	);
 });
