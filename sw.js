6c6a193d0783882e0868a18f6affc9144839f6a5
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2023-07-07 11:10:43 -0300

    .

---
+++ sw.js
@@ -0,0 +1,34 @@
+// Establish a cache name
+const cacheName = 'MyFancyCacheName_v1';
+
+// Assets to precache
+const precachedAssets = [
+  '/',
+  'index.html',
+  'style.css',
+  'app.js',
+  'logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  // Precache assets on install
+  event.waitUntil(caches.open(cacheName).then((cache) => {
+    return cache.addAll(precachedAssets);
+  }));
+});
+
+self.addEventListener('fetch', (event) => {
+  // Is this one of our precached assets?
+  const url = new URL(event.request.url);
+  const isPrecachedRequest = precachedAssets.includes(url.pathname);
+
+  if (isPrecachedRequest) {
+    // Grab the precached asset from the cache
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      return cache.match(event.request.url);
+    }));
+  } else {
+    // Go to the network
+    return;
+  }
+});
