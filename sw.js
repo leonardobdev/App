6f5d250794f55d0840147d26dedbe4b604127e49
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2023-07-07 13:04:55 -0300

    .

--- sw.js
+++ sw.js
@@ -1,34 +1,40 @@
-// Establish a cache name
-const cacheName = 'MyFancyCacheName_v1';
+const cacheName = "MyFancyCacheName_v1";
 
-// Assets to precache
 const precachedAssets = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/logo.png'
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/logo.png"
 ];
 
-self.addEventListener('install', (event) => {
-  // Precache assets on install
+self.addEventListener("install", function() {
   event.waitUntil(caches.open(cacheName).then((cache) => {
     cache.addAll(precachedAssets);
   }));
 });
 
-self.addEventListener('fetch', (event) => {
-  // Is this one of our precached assets?
+self.addEventListener("activate", (event) => {
+  const cacheAllowlist = ["v2"];
+
+  event.waitUntil(
+    caches.forEach((cache, cacheName) => {
+      if (!cacheAllowlist.includes(cacheName)) {
+        return caches.delete(cacheName);
+      }
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
   const isPrecachedRequest = precachedAssets.includes(url.pathname);
 
   if (isPrecachedRequest) {
-    // Grab the precached asset from the cache
     event.respondWith(caches.open(cacheName).then((cache) => {
       return cache.match(event.request.url);
     }));
   } else {
-    // Go to the network
     return;
   }
 });
