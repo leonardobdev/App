f0575a99feb486e26db0ea11f3ecf03e9beaef07
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2024-03-30 02:08:10 -0300

    .

--- sw.js
+++ sw.js
@@ -1,27 +1,29 @@
 var CACHE_ALL = false;
-var APP_NAME = "App"
-var CACHE_VERSION = "0.1"
+var APP_NAME = "App";
+var CACHE_VERSION = "0.1";
 var CACHE_NAME = `${APP_NAME}_v${CACHE_VERSION}`;
 var WHITELIST = [
   "/",
-  "index.html",
-  "manifest.json",
-  "src/js/main.js",
-  "src/css/main.css",
-  "src/img/logo.svg",
-  "src/img/logo.jpg",
-  "src/img/favicon.ico",
-  "src/img/animated_favicon.gif",
-  "src/img/x48.png",
-  "src/img/x72.png",
-  "src/img/x96.png",
-  "src/img/x128.png",
-  "src/img/x192.png",
-  "src/img/x384.png",
-  "src/img/x512.png",
-  "src/img/x1024.png"
-]
+  "/index.html",
+  "/manifest.json",
+  "/src/js/main.js",
+  "/src/css/main.css",
+  "/src/img/logo.svg",
+  "/src/img/logo.jpg",
+  "/src/img/favicon.ico",
+  "/src/img/animated_favicon.gif",
+  "/src/img/x48.png",
+  "/src/img/x72.png",
+  "/src/img/x96.png",
+  "/src/img/x128.png",
+  "/src/img/x192.png",
+  "/src/img/x384.png",
+  "/src/img/x512.png",
+  "/src/img/x1024.png",
+];
 var BLACKLIST = [];
+
+[WHITELIST, BLACKLIST].map(l =>  l.map((v) => "/" + APP_NAME + v));
 
 self.addEventListener("fetch", function (e) {
   e.respondWith(
