f66eaede339299cd34afdae372cedd79a118591b
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2024-03-30 02:23:46 -0300

    .

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,39 @@
-try {
+window.addEventListener("load", function () {
+  var isOnline = true;
+  var isWebAPK = window.matchMedia("(display-mode: standalone)").matches;
+  console.log("[*] The app is running as a " + (isWebAPK ? "WebAPK" : "Browser-Page"));
 
-  window.addEventListener("load", function () {
-    var isOnline = true;
-    var isWebAPK = window.matchMedia("(display-mode: standalone)").matches;
-    console.log("[*] The app is running as a " + (isWebAPK ? "WebAPK" : "Browser-Page"));
-
-    function checkOnlineStatus() {
-      isOnline = navigator.onLine;
-      console.log("[*] Connection status: " + (isOnline ? "online" : "offline"));
-      if (isOnline) {
-        console.log("[*] You are online!");
-      }
-      else {
-        console.log("[*] You are offline!");
-      }
+  function checkOnlineStatus() {
+    isOnline = navigator.onLine;
+    console.log("[*] Connection status: " + (isOnline ? "online" : "offline"));
+    if (isOnline) {
+      console.log("[*] You are online!");
     }
+    else {
+      console.log("[*] You are offline!");
+    }
+  }
 
-    function init() {
-      if (navigator.serviceWorker) {
-        console.log("[*] Register serviceWorker ...");
-        navigator.serviceWorker.register("/App/sw.js", { scope: "/App/" }).then(function (registration) {
-          console.log("[*] ServiceWorker registration successful with scope: ", registration.scope);
-        }, function (err) {
-          console.log("[*] ServiceWorker registration failed: ", err);
-        });
-      } else {
-        console.log("[*] ServiceWorker not supported by your browser!");
-      }
-
-      window.addEventListener("beforeinstallprompt", function () {
-        console.log("[*] WebAPK install event fired!");
+  function init() {
+    if (navigator.serviceWorker) {
+      console.log("[*] Register serviceWorker ...");
+      navigator.serviceWorker.register("/App/sw.js", { scope: "/App/" }).then(function (registration) {
+        console.log("[*] ServiceWorker registration successful with scope: ", registration.scope);
+      }, function (err) {
+        console.log("[*] ServiceWorker registration failed: ", err);
       });
-
-      checkOnlineStatus();
-      window.addEventListener("online", checkOnlineStatus);
-      window.addEventListener("offline", checkOnlineStatus);
+    } else {
+      console.log("[*] ServiceWorker not supported by your browser!");
     }
 
-    init();
-  });
+    window.addEventListener("beforeinstallprompt", function () {
+      console.log("[*] WebAPK install event fired!");
+    });
 
-} catch (err) {
-  console.log(err);
-}
+    checkOnlineStatus();
+    window.addEventListener("online", checkOnlineStatus);
+    window.addEventListener("offline", checkOnlineStatus);
+  }
+
+  init();
+});
--- sw.js
+++ sw.js
@@ -1,67 +1,60 @@
-try {
+var CACHE_ALL = false;
+var APP_NAME = "App";
+var CACHE_VERSION = "0.1";
+var CACHE_NAME = `${APP_NAME}_v${CACHE_VERSION}`;
+var WHITELIST = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/src/js/main.js",
+  "/src/css/main.css",
+  "/src/img/logo.svg",
+  "/src/img/logo.jpg",
+  "/src/img/favicon.ico",
+  "/src/img/animated_favicon.gif",
+  "/src/img/x48.png",
+  "/src/img/x72.png",
+  "/src/img/x96.png",
+  "/src/img/x128.png",
+  "/src/img/x192.png",
+  "/src/img/x384.png",
+  "/src/img/x512.png",
+  "/src/img/x1024.png",
+];
+var BLACKLIST = [];
 
-  var CACHE_ALL = false;
-  var APP_NAME = "App";
-  var CACHE_VERSION = "0.1";
-  var CACHE_NAME = `${APP_NAME}_v${CACHE_VERSION}`;
-  var WHITELIST = [
-    "/",
-    "/index.html",
-    "/manifest.json",
-    "/src/js/main.js",
-    "/src/css/main.css",
-    "/src/img/logo.svg",
-    "/src/img/logo.jpg",
-    "/src/img/favicon.ico",
-    "/src/img/animated_favicon.gif",
-    "/src/img/x48.png",
-    "/src/img/x72.png",
-    "/src/img/x96.png",
-    "/src/img/x128.png",
-    "/src/img/x192.png",
-    "/src/img/x384.png",
-    "/src/img/x512.png",
-    "/src/img/x1024.png",
-  ];
-  var BLACKLIST = [];
+[WHITELIST, BLACKLIST] = [WHITELIST, BLACKLIST].map(l => l.map(v => "/" + APP_NAME + v));
 
-  WHITELIST.map(v => "/" + APP_NAME + v);
-  BLACKLIST.map(v => "/" + APP_NAME + v);
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      return request || fetch(e.request);
+    })
+  )
+});
 
-  self.addEventListener("fetch", function (e) {
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        return request || fetch(e.request);
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("[SW] installing cache : " + CACHE_NAME);
+      return cache.addAll(WHITELIST)
+    })
+  )
+});
+
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_NAME)
       })
-    )
-  });
-
-  self.addEventListener("install", function (e) {
-    e.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache) {
-        console.log("[SW] installing cache : " + CACHE_NAME);
-        return cache.addAll(WHITELIST)
-      })
-    )
-  });
-
-  self.addEventListener("activate", function (e) {
-    e.waitUntil(
-      caches.keys().then(function (keyList) {
-        var cacheWhitelist = keyList.filter(function (key) {
-          return key.indexOf(APP_NAME)
-        })
-        cacheWhitelist.push(CACHE_NAME)
-        return Promise.all(keyList.map(function (key, i) {
-          if (cacheWhitelist.indexOf(key) === -1) {
-            console.log("[SW] deleting cache : " + keyList[i]);
-            return caches.delete(keyList[i]);
-          }
-        }));
-      })
-    )
-  });
-
-} catch (err) {
-  console.log(err);
-}
+      cacheWhitelist.push(CACHE_NAME)
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log("[SW] deleting cache : " + keyList[i]);
+          return caches.delete(keyList[i]);
+        }
+      }));
+    })
+  )
+});
