d7c0c1f08831e1a5a202209700d3a873c3573256
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2024-03-29 23:34:02 -0300

    .

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,10 @@ window.addEventListener("load", function() {
     isOnline = navigator.onLine;
     console.log("[*] Connection status: "+(isOnline?"online":"offline"));
     if (isOnline){
-      $("#connection abbr").html("&#128246;");
-      $("#connection abbr").attr("title","You are online!");
+      console.log("You are online!");
     }
     else {
-      $("#connection abbr").html("&#9888;");
-      $("#connection abbr").attr("title","You are offline!");
+      console.log("title","You are offline!");
     }
   }
   
@@ -29,7 +27,7 @@ window.addEventListener("load", function() {
     
     window.addEventListener("beforeinstallprompt",function(event){
       console.log("[*] WebAPK install event fired!");
-      var btn = $("<button style='display: none;'>install</button>");
+      var btn = "<button style='display: none;'>install</button>";
       document.body.append(btn);
       btn.click(function(e){
         event.prompt();
