f1a8d0e2dbb115b25d07f6a77c85d0157ff80777
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2024-03-30 04:38:54 -0300

    .

--- sw.js
+++ sw.js
@@ -24,65 +24,23 @@ var WHITELIST = [
 var BLACKLIST = [];
 [WHITELIST, BLACKLIST] = [WHITELIST, BLACKLIST].map(l => l.map(v => "/" + APP_NAME + v));
 
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open(CACHE_NAME);
-  await cache.addAll(resources);
-};
-
-const putInCache = async (request, response) => {
-  const cache = await caches.open(CACHE_NAME);
-  await cache.put(request, response);
-};
-
-const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
-  // First try to get the resource from the cache
-  const responseFromCache = await caches.match(request);
-  if (responseFromCache) {
-    return responseFromCache;
-  }
-
-  // Next try to use (and cache) the preloaded response, if it's there
-  const preloadResponse = await preloadResponsePromise;
-  if (preloadResponse) {
-    console.info("using preload response", preloadResponse);
-    putInCache(request, preloadResponse.clone());
-    return preloadResponse;
-  }
-
-  // Next try to get the resource from the network
-  try {
-    const responseFromNetwork = await fetch(request);
-    // response may be used only once
-    // we need to save clone to put one copy in cache
-    // and serve second one
-    putInCache(request, responseFromNetwork.clone());
-    return responseFromNetwork;
-  } catch (error) {
-    const fallbackResponse = await caches.match(fallbackUrl);
-    if (fallbackResponse) {
-      return fallbackResponse;
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((response) => {
+    if (response) {
+      // Verifica se o cache é identico
+      const etag = response.headers.get('ETag');
+      const requestEtag = event.request.headers.get('If-None-Match');
+      if (etag === requestEtag) {
+        return response;
+      } else {
+        // O cache não é identico, deleta e busca a atualização
+        caches.delete(CACHE_NAME, event.request).then(() => {
+          return fetch(event.request);
+        });
+      }
+    } else {
+      // O recurso não está em cache, busca da rede
+      return fetch(event.request);
     }
-    // when even the fallback response is not available,
-    // there is nothing we can do, but we must always
-    // return a Response object
-    return new Response("Network error happened", {
-      status: 408,
-      headers: { "Content-Type": "text/plain" },
-    });
-  }
-};
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    addResourcesToCache(WHITELIST),
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    cacheFirst({
-      request: event.request,
-      preloadResponsePromise: event.preloadResponse
-    }),
-  );
+  }));
 });
