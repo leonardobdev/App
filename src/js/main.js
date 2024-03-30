20f07ff672c3553a86d5454e04295a65b7d68edd
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2024-03-30 02:17:17 -0300

    .

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,45 @@
-window.addEventListener("load", function () {
-  var isOnline = true;
-  var isWebAPK = window.matchMedia("(display-mode: standalone)").matches;
-  console.log("[*] The app is running as a " + (isWebAPK ? "WebAPK" : "Browser-Page"));
+try {
 
-  function checkOnlineStatus() {
-    isOnline = navigator.onLine;
-    console.log("[*] Connection status: " + (isOnline ? "online" : "offline"));
-    if (isOnline) {
-      console.log("[*] You are online!");
-    }
-    else {
-      console.log("[*] You are offline!");
-    }
-  }
+  window.addEventListener("load", function () {
+    var isOnline = true;
+    var isWebAPK = window.matchMedia("(display-mode: standalone)").matches;
+    console.log("[*] The app is running as a " + (isWebAPK ? "WebAPK" : "Browser-Page"));
 
-  function init() {
-    if (navigator.serviceWorker) {
-      console.log("[*] Register serviceWorker ...");
-      navigator.serviceWorker.register("/App/sw.js", { scope: "/App/" }).then(function (registration) {
-        console.log("[*] ServiceWorker registration successful with scope: ", registration.scope);
-      }, function (err) {
-        console.log("[*] ServiceWorker registration failed: ", err);
+    function checkOnlineStatus() {
+      isOnline = navigator.onLine;
+      console.log("[*] Connection status: " + (isOnline ? "online" : "offline"));
+      if (isOnline) {
+        console.log("[*] You are online!");
+      }
+      else {
+        console.log("[*] You are offline!");
+      }
+    }
+
+    function init() {
+      if (navigator.serviceWorker) {
+        console.log("[*] Register serviceWorker ...");
+        navigator.serviceWorker.register("/App/sw.js", { scope: "/App/" }).then(function (registration) {
+          console.log("[*] ServiceWorker registration successful with scope: ", registration.scope);
+        }, function (err) {
+          console.log("[*] ServiceWorker registration failed: ", err);
+        });
+      } else {
+        console.log("[*] ServiceWorker not supported by your browser!");
+      }
+
+      window.addEventListener("beforeinstallprompt", function () {
+        console.log("[*] WebAPK install event fired!");
       });
-    } else {
-      console.log("[*] ServiceWorker not supported by your browser!");
+
+      checkOnlineStatus();
+      window.addEventListener("online", checkOnlineStatus);
+      window.addEventListener("offline", checkOnlineStatus);
     }
 
-    window.addEventListener("beforeinstallprompt", function () {
-      console.log("[*] WebAPK install event fired!");
-    });
+    init();
+  });
 
-    checkOnlineStatus();
-    window.addEventListener("online", checkOnlineStatus);
-    window.addEventListener("offline", checkOnlineStatus);
-  }
-
-  init();
-});
+} catch (err) {
+  console.log(err);
+}
