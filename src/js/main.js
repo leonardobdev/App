7f44d92faeacad19e1b87cc126e3bca2765a8b5e
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2024-03-29 23:27:52 -0300

    .

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,44 @@
-
 var isOnline = true;
 var isWebAPK = window.matchMedia('(display-mode: standalone)').matches;
+console.log("[*] The app is running as a "+(isWebAPK?"WebAPK":"Browser-Page"));
 
-if ('serviceWorker' in navigator) {
-  if (navigator.serviceWorker) {
-    console.log("Register serviceWorker ...");
-    navigator.serviceWorker.register('/App/src/js/sw.js', { scope: '/App/' }).then(function (registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function (err) {
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  }
+function checkOnlineStatus(){
+	isOnline = navigator.onLine;
+	console.log("[*] Connection status: "+(isOnline?"online":"offline"));
+	if (isOnline){
+		$("#connection abbr").html("&#128246;");
+		$("#connection abbr").attr("title","You are online!");
+	}
+	else {
+		$("#connection abbr").html("&#9888;");
+		$("#connection abbr").attr("title","You are offline!");
+	}
 }
-else console.log("ServiceWorker not supported by your browser!");
+
+function init(){
+	if ('serviceWorker' in navigator) {
+		console.log("[*] Register serviceWorker ...");
+		navigator.serviceWorker.register('/App/src/js/sw.js', { scope: '/App/' }).then(function(registration) {
+			console.log('[*] ServiceWorker registration successful with scope: ', registration.scope);
+		}, function(err) {
+			console.log('[*] ServiceWorker registration failed: ', err);
+		});
+	}
+	else console.log("[*] ServiceWorker not supported by your browser!");
+	
+	window.addEventListener("beforeinstallprompt",function(event){
+		console.log("[*] WebAPK install event fired!");
+		var btn = $("<button style='display: none;'>install</button>");
+		$("body").append(btn);
+		btn.click(function(e){
+			event.prompt();
+			btn.remove();
+		});
+	});
+	
+	checkOnlineStatus();
+	window.addEventListener("online",checkOnlineStatus);
+	window.addEventListener("offline",checkOnlineStatus);
+}
+
+init();
