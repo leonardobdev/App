81af45b6a4ab8ec0c595f4e5708957c64881df57
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2024-04-07 05:42:34 -0300

    .

--- sw.js
+++ sw.js
@@ -20,23 +20,49 @@ var URLS = [
     "/src/img/x1024.png",
 ];
 
-self.oninstall = e => e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-        console.log('[sw] installing cache: ' + CACHE_NAME);
-        return cache.addAll(URLS);
-    })
-);
+self.oninstall = async () => {
+    console.log('[sw] installing cache: ' + CACHE_NAME);
+    let cache = await caches.open(CACHE_NAME);
+    let response = await cache.addAll(URLS);
+    e.waitUntil(response);
+    return;
+};
 
 URLS = URLS.map((v) => "/" + CACHE_NAME + v);
 
-self.onfetch = e => e.respondWith(
-    caches.match(e.request).then((request) => {
-        if (request) {
-            console.log('[sw] fetching from cache: ' + e.request.url);
-            return request;
-        } else {
-            console.log('[sw] fetching from network: ' + e.request.url);
-            return fetch(e.request);
+self.onfetch = async event => {
+    let request = event.request;
+    let cachedResponse = await caches.match(request);
+    let updatedResponse = await fetch(request);
+    let response = '';
+    
+    if (cachedResponse) {
+
+        if (updatedResponse.status === 200) {
+
+            let cachedEtag = cachedResponse.headers.get('etag');
+            let updatedEtag = updatedResponse.headers.get('etag');
+
+            if (cachedEtag === updatedEtag) {
+
+                response = cachedResponse
+
+            } else {
+
+                response = updatedResponse;
+                await caches.delete(request);
+                await caches.add(request, updatedResponse);
+
+            }
+
         }
-    })
-);
+
+    } else {
+        
+        response = updatedResponse;
+        
+    }
+
+    event.respondWith(response);
+    return;
+};
